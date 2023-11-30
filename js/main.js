f1ef18eec4d7a31fd46fbf84be4bfcc154eda00f
Md Minhaj <parsonal322532@gmail.com>
2023-11-30 18:00:47 +0600

    Compate Dynamic

--- js/main.js
+++ js/main.js
@@ -1,71 +1,86 @@
-function searchArea(){
-    const searchFilud = document.getElementById('search-filud');
-    return searchFilud.value;
-}
-
-const dataLoad = () =>{
-    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchArea()}`)
+const dataLoad = (word) =>{
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
     .then(res => res.json())
-    .then(data => searchBtn(data))
+    .then(data => searchBtn(data[0]))
 }
 
-const searchBtn = (data)=>{
-    // KeyWord
-    const keyWord = document.getElementById('keyworld');
-    if(searchArea() === data[0].word){
-        keyWord.innerText = data[0].word;
-    }
-    // phonetic Area
-    const phonetic = document.getElementById('phonetic');
-    if(data[0].phonetic !== undefined){
-        phonetic.innerText = data[0].phonetic;
-    }
-    else{
-        phonetic.innerText = 'None';
-    }
-    // Noun Area
-    const nounAndVerb = (elementID, elementNO)=>{
-        const noun = document.getElementById(elementID);
-        noun.innerText = data[0].meanings[elementNO].partOfSpeech;
-    }
-    nounAndVerb('noun', 0);
-    // meaning Area
-    const meaningElement = (meaningElementId, indexNo)=>{
-        const meaningElement1 = document.getElementById(meaningElementId);
-        meaningElement1.innerText = data[0].meanings[0].definitions[indexNo].definition;
-    }
-    meaningElement('meaning-id1', 0);
-    meaningElement('meaning-id2', 1);
-    meaningElement('meaning-id3', 2);
-    // synonyms
-    const synonyms = document.getElementById('synonyms');
-    synonyms.innerText = data[0].meanings[0].synonyms[0];
-    // Verb
-    nounAndVerb('verb', 1);
-    // Meaning Bottom
-    const meaningBottom = document.getElementById('meaning-bottom');
-    meaningBottom.innerText = data[0].meanings[1].definitions[0].definition;
-    // meaning Bottom Sub
-    const meaningBottomSub = document.getElementById('meaning-bottom-sub');
-    if(data[0].meanings[1].definitions[0].example !== undefined){
-        meaningBottomSub.innerText = data[0].meanings[1].definitions[0].example;
-    }
-    // source
-    const source = document.getElementById('source');
-    source.innerText = data[0].sourceUrls[0];
-
+const newElement = (elementId, elementValue) =>{
+    document.getElementById(elementId).innerText = elementValue ? elementValue: '';
 }
 
-const loadAudioData = () =>{
-    const keyworld = document.getElementById('keyworld');
-    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${keyworld.innerText}`)
-    .then(res => res.json())
-    .then(data => audioPlay(data))
+const searchBtn = (data) =>{
+    if(data === undefined){
+       return alert('Enter Type Maning Word')
+    }
+
+    newElement('word', data.word);
+    newElement('word-phonetic', data.phonetic);
+    newElement('source-link', data.sourceUrls[0]);
+    
+    const wordHeader = document.getElementById('word-header');
+    wordHeader.innerText = '';
+    const audioBtn = document.createElement('button');
+    audioBtn.id = 'audio-btn';
+    audioBtn.classList.add('h-[48px]', 'w-[48px]', 'md:h-[75px]', 'md:w-[75px]', 'bg-contain', 'rounded-full', "bg-[url('./public/assets/icon-play.svg')]")
+    wordHeader.appendChild(audioBtn);
+
+    const audio = () =>{
+        const allAudio = data.phonetics;
+        for(const audios of allAudio){
+            if(audios.audio !== ''){
+                const play = new Audio(audios.audio);
+                play.play();
+                return;
+            }
+        }
+    }
+
+    document.getElementById('audio-btn').addEventListener('click', () =>{
+        console.log('Clickd');
+        audio();
+    })
+
+    const detailsContainer = document.getElementById('detail-container');
+    const wordMeanings = data.meanings;
+    for(let i = 0; i < wordMeanings.length; i++){
+        const wordElement = wordMeanings[i];
+
+        const partOfSpeech = document.createElement('section');
+        partOfSpeech.innerHTML = `
+        <section class="mb-10">
+        <div class="flex items-center gap-1 mb-8">
+            <p class="font-[Courgette] font-bold text-xl">${wordElement.partOfSpeech}</p><hr class="inline-block basis-full">
+        </div>
+        <div class="word-details">
+            <p class="mb-4 text-stone-500 text-xl">Meaning</p>
+            <ul id="details-list-${i}" class="ms-12 list-disc mb-4">
+                
+            <li class="mb-3 text-lg font-medium">(etc.) A set of keys used to operate a typewriter, computer etc.</li>
+
+            </ul>
+            <div class="flex flex-wrap mt-8">
+                <p class="mr-4 text-stone-500 text-xl">Synonyms</p>
+                <p class="text-purple-500 mr-1 font-bold text-xl">electronic keyboard</p>
+            </div>
+        </div>
+        </section>
+        `;
+        
+        detailsContainer.appendChild(partOfSpeech);
+        const detailsList = document.getElementById(`details-list-${i}`);
+        const defintions = wordElement.definitions;
+        for(const defintion of defintions){
+            const newElement = document.createElement('li');
+            newElement.classList.add('mb-3', 'text-lg',  'font-medium');
+            newElement.innerText = defintion.definition;
+            detailsList.appendChild(newElement);
+        }
+    }
 }
 
-const audioPlay = (data) =>{
-    const audio = document.getElementById('audio');
-    // audio.src = data[0].phonetics[2].audio;
-    console.log(data[0].word);
-    console.log(data[0].phonetics[2]);
-}
+
+document.getElementById('search').addEventListener('change', function(){
+    dataLoad(this.value);
+})
+
+dataLoad('keyboard')
